996b9b40414342e0ede50560a23155d344bf8b67
Oscar Nord <oscar.nord@gmail.com>
2023-03-25 23:06:09 +0100

    fixup! fix tests

--- src/api.test.ts
+++ src/api.test.ts
@@ -2,12 +2,12 @@ import api from './api';
 
 describe('api', () => {
   it('responds with hello, world!', async () => {
-    const server = api({ title: 'my awesome service' });
+    const server = api({ title: '@eyevinn/subtitle-generator' });
     const response = await server.inject({
       method: 'GET',
       url: '/'
     });
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('Hello, world! I am my awesome service');
+    expect(response.body).toBe('@eyevinn/subtitle-generator is healthy 💖');
   });
 });
