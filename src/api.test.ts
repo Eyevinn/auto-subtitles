864c4313480e40c6cd00285b891322b711af5460
oscnord <6131004+oscnord@users.noreply.github.com>
2024-02-08 10:13:08 +0100

    fix: update name of service

--- src/api.test.ts
+++ src/api.test.ts
@@ -2,12 +2,12 @@ import api from './api';
 
 describe('api', () => {
   it('responds with hello, world!', async () => {
-    const server = api({ title: '@eyevinn/subtitle-generator' });
+    const server = api({ title: '@eyevinn/auto-subtitles' });
     const response = await server.inject({
       method: 'GET',
       url: '/'
     });
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('@eyevinn/subtitle-generator is healthy 💖');
+    expect(response.body).toBe('@eyevinn/auto-subtitles is healthy 💖');
   });
 });
